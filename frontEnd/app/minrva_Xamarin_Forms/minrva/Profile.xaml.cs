79cac52f7fceb96bba155aeccfaaccf18693eabb
Nikhita <nv214@ic.ac.uk>
2016-10-15 19:20:25 +0100

    Added profile tab

---
+++ frontEnd/app/minrva_Xamarin_Forms/minrva/Profile.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace minrva
+{
+	public partial class Profile : ContentPage
+	{
+		public Profile()
+		{
+			InitializeComponent();
+		}
+	}
+}
