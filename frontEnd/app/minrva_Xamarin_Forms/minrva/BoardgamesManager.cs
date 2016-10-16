09996681377f941e85fe7e7be492d779c02f4d52
Nikhita <nv214@ic.ac.uk>
2016-10-16 17:57:25 +0100

    Connecting insert item page to board games database table

--- frontEnd/app/minrva_Xamarin_Forms/minrva/BoardgamesManager.cs
+++ frontEnd/app/minrva_Xamarin_Forms/minrva/BoardgamesManager.cs
@@ -1,10 +1,162 @@
-﻿using System;
+﻿/*
+ * To add Offline Sync Support:
+ *  1) Add the NuGet package Microsoft.Azure.Mobile.Client.SQLiteStore (and dependencies) to all client projects
+ *  2) Uncomment the #define OFFLINE_SYNC_ENABLED
+ *
+ * For more information, see: http://go.microsoft.com/fwlink/?LinkId=620342
+ */
+//#define OFFLINE_SYNC_ENABLED
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+#if OFFLINE_SYNC_ENABLED
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+#endif
+
 namespace minrva
 {
-	public class BoardgamesManager
+	public partial class BoardgamesManager
 	{
-		public BoardgamesManager()
+		static BoardgamesManager defaultInstance = new BoardgamesManager();
+		MobileServiceClient client;
+
+#if OFFLINE_SYNC_ENABLED
+        IMobileServiceSyncTable<Boardgames> BoardgamesTable;
+#else
+		IMobileServiceTable<Boardgames> boardgamesTable;
+#endif
+
+		const string offlineDbPath = @"localstore.db";
+
+		private BoardgamesManager()
 		{
+			this.client = new MobileServiceClient(Constants.ApplicationURL);
+
+#if OFFLINE_SYNC_ENABLED
+            var store = new MobileServiceSQLiteStore(offlineDbPath);
+            store.DefineTable<Boardgames>();
+
+            //Initializes the SyncContext using the default IMobileServiceSyncHandler.
+            this.client.SyncContext.InitializeAsync(store);
+
+            this.boardgamesTable = client.GetSyncTable<Boardgames>();
+#else
+			this.boardgamesTable = client.GetTable<Boardgames>();
+#endif
 		}
+
+		public static BoardgamesManager DefaultManager
+		{
+			get
+			{
+				return defaultInstance;
+			}
+			private set
+			{
+				defaultInstance = value;
+			}
+		}
+
+		public MobileServiceClient CurrentClient
+		{
+			get { return client; }
+		}
+
+		public bool IsOfflineEnabled
+		{
+			get { return boardgamesTable is Microsoft.WindowsAzure.MobileServices.Sync.IMobileServiceSyncTable<Boardgames>; }
+		}
+
+		public async Task<ObservableCollection<Boardgames>> GetBoardgamesAsync(bool syncItems = false)
+		{
+			try
+			{
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncAsync();
+                }
+#endif
+				IEnumerable<Boardgames> items = await boardgamesTable
+					.ToEnumerableAsync();
+
+				return new ObservableCollection<Boardgames>(items);
+			}
+			catch (MobileServiceInvalidOperationException msioe)
+			{
+				Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine(@"Sync error: {0}", e.Message);
+			}
+			return null;
+		}
+
+		public async Task SaveTaskAsync(Boardgames item)
+		{
+			if (item.Id == null)
+			{
+				await boardgamesTable.InsertAsync(item);
+			}
+			else
+			{
+				await boardgamesTable.UpdateAsync(item);
+			}
+		}
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.boardgamesTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allBoardgames",
+                    this.boardgamesTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
 	}
 }
