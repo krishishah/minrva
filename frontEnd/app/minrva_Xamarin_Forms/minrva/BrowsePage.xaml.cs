d49aeed7ee6a13a3c0d85e336c69df4b619bea8b
Nikhita <nv214@ic.ac.uk>
2016-10-16 15:19:56 +0100

    Search bar now prints item searched for

--- frontEnd/app/minrva_Xamarin_Forms/minrva/BrowsePage.xaml.cs
+++ frontEnd/app/minrva_Xamarin_Forms/minrva/BrowsePage.xaml.cs
@@ -7,9 +7,38 @@ namespace minrva
 {
 	public partial class BrowsePage : ContentPage
 	{
+		Label resultsLabel;
+		SearchBar searchBar;
 		public BrowsePage()
 		{
-			InitializeComponent();
+			resultsLabel = new Label
+			{
+				Text = "Result will appear here.",
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				FontSize = 25
+			};
+
+			searchBar = new SearchBar
+			{
+				Placeholder = "Browse items, users, categories",
+				SearchCommand = new Command(() => { 
+					resultsLabel.Text = "You searched for " + searchBar.Text; 
+				})
+			};
+
+			Content = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Start,
+				Children = {
+					searchBar,
+					new ScrollView
+					{
+						Content = resultsLabel,
+						VerticalOptions = LayoutOptions.FillAndExpand
+					}
+				},
+				Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5)
+			};
 		}
 	}
 }
