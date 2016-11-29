60294f07e54c9ea5ac0cbacddeb494a15871c6fe
Srikrishna Murali Subrahmanyam <themsrikrishna@gmail.com>
2016-11-29 14:51:16 +0000

    Created vouch table class

---
+++ frontEnd/app/minrva_Xamarin_Forms/minrva/Tables/Vouch.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace minrva
+{
+	public class Vouch
+	{		
+		string voucher;
+		string vouchee;
+		string createdAt;
+		string updatedAt;
+
+		[JsonProperty(PropertyName = "voucher")]
+		public string Voucher
+		{
+			get { return voucher; }
+			set { voucher = value; }
+		}
+
+		[JsonProperty(PropertyName = "vouchee")]
+		public string Vouchee
+		{
+			get { return vouchee; }
+			set { vouchee = value; }
+		}
+
+		[JsonProperty(PropertyName = "createdAt")]
+		public string CreatedAt
+		{
+			get { return createdAt; }
+			set { createdAt = value; }
+		}
+
+		[JsonProperty(PropertyName = "updatedAt")]
+		public string UpdatedAt
+		{
+			get { return updatedAt; }
+			set { updatedAt = value; }
+		}
+
+	}
+}
+
