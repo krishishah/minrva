48bfae2e049d49ecb10b0eafdb9dbe3f4176ca2b
Srikrishna Murali Subrahmanyam <themsrikrishna@gmail.com>
2016-12-02 00:22:33 +0000

    Created list of users who vouch for current user

--- frontEnd/app/minrva_Xamarin_Forms/minrva/AppPages/VoucheesList.xaml.cs
+++ frontEnd/app/minrva_Xamarin_Forms/minrva/AppPages/VoucheesList.xaml.cs
@@ -45,13 +45,13 @@ namespace minrva
 			var sid = await App.Authenticator.GetUserId();
 			var vouchTable = await tableManager.GetVouchAsync();
 			var userTable = await tableManager.GetUserAsync();
-			var currentUserVouchList = vouchTable.Where(owner => String.Equals(sid, owner.Voucher));
+			var currentUserVouchList = vouchTable.Where(owner => String.Equals(sid, owner.Vouchee));
 
 			List<User> vouchNetwork = new List<User>();
 
 			foreach (Vouch v in currentUserVouchList)
 			{
-				User vouchee = userTable.Where(u => String.Equals(u.UserId, v.Vouchee)).ElementAt(0);
+				User vouchee = userTable.Where(u => String.Equals(u.UserId, v.Voucher)).ElementAt(0);
 				vouchNetwork.Add(vouchee);
 			}
 
